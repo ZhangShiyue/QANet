6788b002c290317a69e07c43fe7ef2a16a8b608d
Zhang Shiyue <yuer3677@163.com>
2018-11-01 13:22:38 -0400

    decoder layer 2 model encoder layer 2 hidden 512

--- config.py
+++ config.py
@@ -27,10 +27,10 @@ if not os.path.exists(train_dir):
 if not os.path.exists(os.path.join(os.getcwd(),dir_name)):
     os.mkdir(os.path.join(os.getcwd(),dir_name))
 target_dir = "data_new21"
-log_dir = os.path.join(dir_name, "event_qg42")
-save_dir = os.path.join(dir_name, "model_qg42")
+log_dir = os.path.join(dir_name, "event_qg43")
+save_dir = os.path.join(dir_name, "model_qg43")
 save_dir_dual = os.path.join(dir_name, "model_qa")
-answer_dir = os.path.join(dir_name, "answer_qg42")
+answer_dir = os.path.join(dir_name, "answer_qg43")
 train_record_file = os.path.join(target_dir, "train.tfrecords")
 dev_record_file = os.path.join(target_dir, "dev.tfrecords")
 test_record_file = os.path.join(target_dir, "test.tfrecords")
@@ -113,7 +113,7 @@ flags.DEFINE_float("ml_learning_rate", 0.001, "Learning rate")
 flags.DEFINE_float("rl_learning_rate", 0.00001, "Learning rate")
 flags.DEFINE_float("decay", None, "Exponential moving average decay")
 flags.DEFINE_float("l2_norm", 3e-07, "L2 norm scale")
-flags.DEFINE_integer("hidden", 256, "Hidden size")
+flags.DEFINE_integer("hidden", 512, "Hidden size")
 flags.DEFINE_integer("num_heads", 1, "Number of heads in self attention")
 flags.DEFINE_integer("early_stop", 10, "Checkpoints for early stop")
 
