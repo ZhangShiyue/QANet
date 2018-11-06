00e94566ce6db8053fea0a3d99cc2c43e65ed0b0
Zhang Shiyue <yuer3677@163.com>
2018-11-06 14:39:52 -0500

    old baseline with que vocab

--- prepro3.py
+++ prepro3.py
@@ -6,7 +6,6 @@ import ujson as json
 from collections import Counter
 import numpy as np
 from codecs import open
-import pickle as pkl
 
 '''
 This file is taken and modified from R-Net by HKUST-KnowComp
@@ -34,8 +33,7 @@ def convert_idx(text, tokens):
     return spans
 
 
-def process_file(filename, data_type, word_counter=None, que_word_counter=None, char_counter=None,
-                 lower_word=False, titles=None):
+def process_file(filename, data_type, word_counter, char_counter, lower_word=False):
     print("Generating {} examples...".format(data_type))
     examples = []
     eval_examples = {}
@@ -44,8 +42,6 @@ def process_file(filename, data_type, word_counter=None, que_word_counter=None, 
     with open(filename, "r") as fh:
         source = json.load(fh)
         for article in tqdm(source["data"]):
-            if titles is not None and article["title"].encode('utf-8') not in titles:
-                continue
             for para in article["paragraphs"]:
                 context = para["context"].replace(
                         "''", '" ').replace("``", '" ').replace(u'\u2013', '-')
@@ -55,11 +51,10 @@ def process_file(filename, data_type, word_counter=None, que_word_counter=None, 
                 max_c = max(max_c, len(context_tokens))
                 context_chars = [list(token) for token in context_tokens]
                 spans = convert_idx(context, context_tokens)
-                if word_counter is not None:
-                    for token in context_tokens:
-                        word_counter[token] += len(para["qas"])
-                        for char in token:
-                            char_counter[char] += len(para["qas"])
+                # for token in context_tokens:
+                #     word_counter[token] += len(para["qas"])
+                #     for char in token:
+                #         char_counter[char] += len(para["qas"])
                 for qa in para["qas"]:
                     total += 1
                     ques = qa["question"].replace(
@@ -69,14 +64,10 @@ def process_file(filename, data_type, word_counter=None, que_word_counter=None, 
                     ques_tokens = ["--GO--"] + word_tokenize(ques) + ["--EOS--"]
                     max_q = max(max_q, len(ques_tokens))
                     ques_chars = [list(token) for token in ques_tokens]
-                    if que_word_counter is not None:
-                        for token in ques_tokens:
-                            que_word_counter[token] += 1
-                    elif word_counter is not None:
-                        for token in ques_tokens:
-                            word_counter[token] += 1
-                            for char in token:
-                                char_counter[char] += 1
+                    for token in ques_tokens:
+                        word_counter[token] += 1
+                        for char in token:
+                            char_counter[char] += 1
                     y1s, y2s = [], []
                     answer_texts = []
                     answer_tokens = []
@@ -243,7 +234,7 @@ def convert_to_features(config, data, word2idx_dict, char2idx_dict):
     return context_idxs, context_char_idxs, ques_idxs, ques_char_idxs
 
 
-def build_features(config, examples, data_type, out_file, word2idx_dict, que_word2idx_dict,
+def build_features(config, examples, data_type, out_file, word2idx_dict,
                    char2idx_dict, is_test=False):
     para_limit = config.test_para_limit if is_test else config.para_limit
     ques_limit = config.test_ques_limit if is_test else config.ques_limit
@@ -288,12 +279,6 @@ def build_features(config, examples, data_type, out_file, word2idx_dict, que_wor
                     return word2idx_dict[each]
             return 1
 
-        def _get_que_word(word, i):
-            for each in (word, word.lower(), word.capitalize(), word.upper()):
-                if each in que_word2idx_dict:
-                    return que_word2idx_dict[each]
-            return 1
-
         def _get_char(char):
             if char in char2idx_dict:
                 return char2idx_dict[char]
@@ -314,11 +299,11 @@ def build_features(config, examples, data_type, out_file, word2idx_dict, que_wor
                 context_idxs_ans[i] = wid
 
         for i, token in enumerate(example["ques_tokens"]):
-            wid = _get_que_word(token, i)
+            wid = _get_word(token, i)
             if config.use_pointer:
-                ques_idxs[i] = len(que_word2idx_dict) + example["context_tokens"].index(token) \
+                ques_idxs[i] = len(word2idx_dict) + example["context_tokens"].index(token) \
                     if wid == 1 and token in example["context_tokens"] else wid
-                ques_idxs_ans[i] = len(que_word2idx_dict) + example["context_tokens_ans"].index(token) \
+                ques_idxs_ans[i] = len(word2idx_dict) + example["context_tokens_ans"].index(token) \
                     if wid == 1 and token in example["context_tokens_ans"] else wid
             else:
                 ques_idxs[i] = wid
@@ -385,16 +370,13 @@ def save(filename, obj, message=None):
 
 
 def prepro(config):
-    train_titles = map(lambda x: x.strip(), open("processed/doclist-train.txt", 'r').readlines())
-    test_titles = map(lambda x: x.strip(), open("processed/doclist-test.txt", 'r').readlines())
-    word_counter, que_word_counter, char_counter = Counter(), Counter(), Counter()
-    train_examples, train_eval = process_file(config.train_file, "train", word_counter=word_counter,
-                                              que_word_counter=que_word_counter, char_counter=char_counter,
-                                              lower_word=config.lower_word, titles=train_titles)
-    dev_examples, dev_eval = process_file(config.dev_file, "dev", word_counter, que_word_counter=que_word_counter,
-                                          char_counter=char_counter, lower_word=config.lower_word)
-    test_examples, test_eval = process_file(config.train_file, "test", lower_word=config.lower_word,
-                                            titles=test_titles)
+    word_counter, char_counter = Counter(), Counter()
+    train_examples, train_eval = process_file(config.train_file, "train", word_counter,
+            char_counter, lower_word=config.lower_word)
+    dev_examples, dev_eval = process_file(config.dev_file, "dev", word_counter,
+            char_counter, lower_word=config.lower_word)
+    test_examples, test_eval = process_file(config.test_file, "test", word_counter,
+            char_counter, lower_word=config.lower_word)
 
     word_emb_file = config.glove_word_file
     char_emb_file = config.glove_char_file if config.pretrained_char else None
@@ -405,23 +387,16 @@ def prepro(config):
                                                 size=config.glove_word_size, vec_size=config.glove_dim,
                                                 limit=config.vocab_count_limit, lower_word=config.lower_word)
     print len(word2idx_dict)
-    que_word_emb_mat, que_word2idx_dict = get_embedding(que_word_counter, "word", emb_file=word_emb_file,
-                                                size=config.glove_word_size, vec_size=config.glove_dim,
-                                                limit=config.vocab_count_limit, lower_word=config.lower_word)
-    print len(que_word2idx_dict)
     char_emb_mat, char2idx_dict = get_embedding(char_counter, "char", emb_file=char_emb_file,
             size=char_emb_size, vec_size=char_emb_dim, limit=config.char_count_limit, lower_word=config.lower_word)
     print len(char2idx_dict)
 
-    build_features(config, train_examples, "train", config.train_record_file, word2idx_dict,
-                   que_word2idx_dict, char2idx_dict)
-    dev_meta = build_features(config, dev_examples, "dev", config.dev_record_file, word2idx_dict,
-                              que_word2idx_dict, char2idx_dict)
-    test_meta = build_features(config, test_examples, "test", config.test_record_file, word2idx_dict,
-                               que_word2idx_dict, char2idx_dict, is_test=True)
+    build_features(config, train_examples, "train", config.train_record_file, word2idx_dict, char2idx_dict)
+    dev_meta = build_features(config, dev_examples, "dev", config.dev_record_file, word2idx_dict, char2idx_dict)
+    test_meta = build_features(config, test_examples, "test", config.test_record_file, word2idx_dict, char2idx_dict,
+                               is_test=True)
 
     save(config.word_emb_file, word_emb_mat, message="word embedding")
-    save(config.que_word_emb_file, que_word_emb_mat, message="que word embedding")
     save(config.char_emb_file, char_emb_mat, message="char embedding")
     save(config.train_eval_file, train_eval, message="train eval")
     save(config.dev_eval_file, dev_eval, message="dev eval")
@@ -429,5 +404,110 @@ def prepro(config):
     save(config.dev_meta, dev_meta, message="dev meta")
     save(config.test_meta, test_meta, message="test meta")
     save(config.word_dictionary, word2idx_dict, message="word dictionary")
-    save(config.que_word_dictionary, que_word2idx_dict, message="que word dictionary")
     save(config.char_dictionary, char2idx_dict, message="char dictionary")
+
+
+def prepro_rerank(config):
+    para_limit = config.test_para_limit
+    ques_limit = config.test_ques_limit
+    ans_limit = config.test_ans_limit
+    char_limit = config.char_limit
+
+    with open(config.beam_search_file, "r") as fh:
+        answer_dict = json.load(fh)
+    with open(config.word_dictionary, "r") as fh:
+        word2idx_dict = json.load(fh)
+    with open(config.char_dictionary, "r") as fh:
+        char2idx_dict = json.load(fh)
+
+    word_counter, char_counter = Counter(), Counter()
+    test_examples, test_eval = process_file(
+         config.test_file, "test", word_counter, char_counter)
+
+    writer = tf.python_io.TFRecordWriter(config.rerank_file)
+
+    total = 0
+    for test_example in test_examples:
+        candidate_answers = answer_dict[str(test_example["id"])]
+        candidate_answer_tokens = []
+        candidate_answer_chars = []
+        for candidate_answer, _, _, _ in candidate_answers:
+            candidate_answer_tokens.append(word_tokenize(candidate_answer))
+            candidate_answer_chars.append([list(token) for token in candidate_answer_tokens[-1]])
+
+        context_idxs = np.zeros([para_limit], dtype=np.int32)
+        context_char_idxs = np.zeros([para_limit, char_limit], dtype=np.int32)
+        # context_voc = np.zeros([len(word2idx_dict) + para_limit], dtype=np.int32)
+        ques_idxs = np.zeros([ques_limit], dtype=np.int32)
+        ans_idxs = [np.zeros([ans_limit], dtype=np.int32) for _ in range(len(candidate_answers))]
+        ques_char_idxs = np.zeros([ques_limit, char_limit], dtype=np.int32)
+        ans_char_idxs = [np.zeros([ans_limit, char_limit], dtype=np.int32) for _ in range(len(candidate_answers))]
+        y1 = np.zeros([para_limit], dtype=np.float32)
+        y2 = np.zeros([para_limit], dtype=np.float32)
+
+        def _get_word(word, i):
+            for each in (word, word.lower(), word.capitalize(), word.upper()):
+                if each in word2idx_dict:
+                    return word2idx_dict[each]
+            return len(word2idx_dict) + i
+
+        def _get_char(char):
+            if char in char2idx_dict:
+                return char2idx_dict[char]
+            return 1
+
+        for i, token in enumerate(test_example["context_tokens"]):
+            wid = _get_word(token, i)
+            context_idxs[i] = wid
+            # if wid < len(word2idx_dict):
+            #     context_voc[wid] = 1
+
+        for i, token in enumerate(test_example["ques_tokens"]):
+            ques_idxs[i] = _get_word(token, i)
+
+        for k, candidate_answer_token in enumerate(candidate_answer_tokens):
+            for i, token in enumerate(candidate_answer_token):
+                ans_idxs[k][i] = _get_word(token, i)
+
+        for k, candidate_answer_char in enumerate(candidate_answer_chars):
+            for i, token in enumerate(candidate_answer_char):
+                for j, char in enumerate(token):
+                    if j == char_limit:
+                        break
+                    ans_char_idxs[k][i, j] = _get_char(char)
+
+        for i, token in enumerate(test_example["context_chars"]):
+            for j, char in enumerate(token):
+                if j == char_limit:
+                    break
+                context_char_idxs[i, j] = _get_char(char)
+
+        for i, token in enumerate(test_example["ques_chars"]):
+            for j, char in enumerate(token):
+                if j == char_limit:
+                    break
+                ques_char_idxs[i, j] = _get_char(char)
+
+        start, end = test_example["y1s"][-1], test_example["y2s"][-1]
+        y1[start], y2[end] = 1.0, 1.0
+
+        for i, (ans_idx, ans_char_idx) in enumerate(zip(ans_idxs, ans_char_idxs)):
+            total += 1
+            record = tf.train.Example(features=tf.train.Features(feature={
+                "context_idxs": tf.train.Feature(bytes_list=tf.train.BytesList(value=[context_idxs.tostring()])),
+                "ques_idxs": tf.train.Feature(bytes_list=tf.train.BytesList(value=[ques_idxs.tostring()])),
+                "ans_idxs": tf.train.Feature(bytes_list=tf.train.BytesList(value=[ans_idx.tostring()])),
+                "context_char_idxs": tf.train.Feature(bytes_list=tf.train.BytesList(value=[context_char_idxs.tostring()])),
+                "ques_char_idxs": tf.train.Feature(bytes_list=tf.train.BytesList(value=[ques_char_idxs.tostring()])),
+                "ans_char_idxs": tf.train.Feature(bytes_list=tf.train.BytesList(value=[ans_char_idx.tostring()])),
+                "y1": tf.train.Feature(bytes_list=tf.train.BytesList(value=[y1.tostring()])),
+                "y2": tf.train.Feature(bytes_list=tf.train.BytesList(value=[y2.tostring()])),
+                "id": tf.train.Feature(int64_list=tf.train.Int64List(value=[test_example["id"]])),
+                "cid": tf.train.Feature(int64_list=tf.train.Int64List(value=[i]))
+            }))
+            writer.write(record.SerializeToString())
+    writer.close()
+    meta = {}
+    print total
+    meta["total"] = total
+    save(config.rerank_meta, meta, message="test meta")
