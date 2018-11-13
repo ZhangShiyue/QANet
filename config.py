36b952f06109cdf7f8a0a55e02270eedd476af32
Zhang Shiyue <yuer3677@163.com>
2018-11-13 11:03:23 -0500

    only with training data

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from prepro import prepro
 from prepro1 import prepro as prepro1
 from prepro2 import prepro as prepro2
 from prepro3 import prepro as prepro3
-# from prepro2_sent import prepro as prepro2_sent
+from prepro2_sent import prepro as prepro2_sent
 from main import train, train_rl, train_dual, test, test_beam, test_bleu, \
     test_rerank, test_reranked, tmp
 
@@ -29,11 +29,11 @@ if not os.path.exists(train_dir):
     os.mkdir(train_dir)
 if not os.path.exists(os.path.join(os.getcwd(),dir_name)):
     os.mkdir(os.path.join(os.getcwd(),dir_name))
-target_dir = "data_new_sent"
-log_dir = os.path.join(dir_name, "event_qg_sent5")
-save_dir = os.path.join(dir_name, "model_qg_sent5")
+target_dir = "data_new_sent3"
+log_dir = os.path.join(dir_name, "event_qg_sent6")
+save_dir = os.path.join(dir_name, "model_qg_sent6")
 save_dir_dual = os.path.join(dir_name, "model_qa")
-answer_dir = os.path.join(dir_name, "answer_qg_sent5")
+answer_dir = os.path.join(dir_name, "answer_qg_sent6")
 train_record_file = os.path.join(target_dir, "train.tfrecords")
 dev_record_file = os.path.join(target_dir, "dev.tfrecords")
 test_record_file = os.path.join(target_dir, "test.tfrecords")
@@ -118,7 +118,7 @@ flags.DEFINE_float("dropout", 0.3, "Dropout prob across the layers")
 flags.DEFINE_float("mixing_ratio", 0.9, "The mixing ratio between ml loss and rl loss")
 flags.DEFINE_float("answer_sup_ratio", None, "The mixing ratio between ml loss and rl loss")
 flags.DEFINE_float("grad_clip", 5.0, "Global Norm gradient clipping rate")
-flags.DEFINE_float("ml_learning_rate", 0.0001, "Learning rate")
+flags.DEFINE_float("ml_learning_rate", 0.001, "Learning rate")
 flags.DEFINE_float("rl_learning_rate", 0.00001, "Learning rate")
 flags.DEFINE_float("decay", None, "Exponential moving average decay")
 flags.DEFINE_float("l2_norm", 3e-07, "L2 norm scale")
@@ -139,7 +139,7 @@ flags.DEFINE_boolean("pretrained_char", False, "Whether to use pretrained charac
 flags.DEFINE_boolean("lower_word", False, "Whether to lower word")
 flags.DEFINE_integer("vocab_count_limit", -1, "Minimum count of words in the vocab")
 flags.DEFINE_integer("char_count_limit", -1, "Minimum count of chars in the char vocab")
-flags.DEFINE_integer("size_limit", 50000, "Minimum count of chars in the char vocab")
+flags.DEFINE_integer("size_limit", -1, "Minimum count of chars in the char vocab")
 flags.DEFINE_integer("glove_word_size", int(2.2e6), "Corpus size for Glove")
 flags.DEFINE_integer("glove_dim", 300, "Embedding dimension for Glove")
 flags.DEFINE_integer("char_dim", 64, "Embedding dimension for char")
